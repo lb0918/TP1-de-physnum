ec21a71f5e4530fb252ea514f43e7cca140ced52
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-02-01 20:03:37 -0500

    petites corrections au 5

--- test.py
+++ test.py
@@ -7,17 +7,17 @@ c = 3e8
 m_p = 1.6726e-27
 Énergie_moyenne_excitation_eau = 75*1.602e-19
 Énergie_moyenne_excitation_os = 91.9*1.602e-19
-rho_eau = 1000
+rho_eau = 997
 rho_os = 1850
 def densité_électronique(composition_atomique, masse_volumique):
     nbr_électrons_volumique = 0
-    avogadro = 6.023e23
-    masse_atomique = {1: 1.007975, 6: 12.0106, 7: 14.006855, 8: 15.9940, 12: 24.3055, 15: 30.97396200, 16: 32.0675, 20: 40.078} 
+    avogadro = 6.022e23
+    masse_atomique = {1: 0.001007975, 6: 0.0120106, 7: 0.014006855, 8: 0.0159940, 12: 0.0243055, 15: 0.03097396200, 16: 0.0320675, 20: 0.040078} 
     for x in composition_atomique:
         nbr_électrons_volumique += masse_volumique * x[1] * avogadro * x[0] / masse_atomique[x[0]]
     return nbr_électrons_volumique
-eau = [(1, 0.111894), (8, 0.888106)]
-densité_électronique_eau = densité_électronique(eau, 1)
+eau = [(1, 0.111894), (8, 0.888106)]#
+densité_électronique_eau = densité_électronique(eau, 997)
 def f_eau(T):
     gamma = T/(m_p*c**2) + 1
     Beta = np.sqrt((gamma**2-1)/gamma**2)
@@ -25,7 +25,7 @@ def f_eau(T):
     b = 1 + (m_e/m_p)**2
     delta = 2*m_e/m_p
     T_emax = (a*(gamma**2-1))/(b+(delta*gamma))
-    S_col_eau = 2 * np.pi * (r_e ** 2)* m_e * (c ** 2) * (densité_électronique_eau*(10**3)) * (1/Beta**2) * (np.log((2*m_e*(c**2)*(Beta**2)*(gamma**2)*T_emax)/((Énergie_moyenne_excitation_eau)**2))-2*(Beta**2))
+    S_col_eau = 2 * np.pi * (r_e ** 2)* m_e * (c ** 2) * (densité_électronique_eau) * (1/Beta**2) * (np.log((2*m_e*(c**2)*(Beta**2)*(gamma**2)*T_emax)/((Énergie_moyenne_excitation_eau)**2))-2*(Beta**2))
     return rho_eau/S_col_eau
 N=1000
 a = 0
