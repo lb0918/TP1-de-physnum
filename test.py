e2219324d791cfd84b84df97a1eaed2df62626fe
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-02-03 12:19:11 -0500

    3 février

--- test.py
+++ test.py
@@ -18,6 +18,8 @@ def densité_électronique(composition_atomique, masse_volumique):
     return nbr_électrons_volumique
 eau = [(1, 0.111894), (8, 0.888106)]#
 densité_électronique_eau = densité_électronique(eau, 997)
+os = [(1,0.063984), (6,0.278000), (7,0.027000), (8,0.410016), (12,0.002), (15,0.07), (16,0.002), (20,0.147)]
+densité_électronique_os = densité_électronique(os, 1850)
 def f_eau(T):
     gamma = T/(m_p*c**2) + 1
     Beta = np.sqrt((gamma**2-1)/gamma**2)
@@ -27,12 +29,74 @@ def f_eau(T):
     T_emax = (a*(gamma**2-1))/(b+(delta*gamma))
     S_col_eau = 2 * np.pi * (r_e ** 2)* m_e * (c ** 2) * (densité_électronique_eau) * (1/Beta**2) * (np.log((2*m_e*(c**2)*(Beta**2)*(gamma**2)*T_emax)/((Énergie_moyenne_excitation_eau)**2))-2*(Beta**2))
     return rho_eau/S_col_eau
-N=1000
-a = 0
-b = T_i
-h = (b - a)/N
+def f_os(T):
+    gamma = T/(m_p*c**2) + 1
+    Beta = np.sqrt((gamma**2-1)/gamma**2)
+    a = 2*m_e*c**2
+    b = 1 + (m_e/m_p)**2
+    delta = 2*m_e/m_p
+    T_emax = (a*(gamma**2-1))/(b+(delta*gamma))
+    S_col_os = 2 * np.pi * (r_e ** 2)* m_e * (c ** 2) * (densité_électronique_os) * (1/Beta**2) * (np.log((2*m_e*(c**2)*(Beta**2)*(gamma**2)*T_emax)/((Énergie_moyenne_excitation_os)**2))-2*(Beta**2))
+    return rho_os/S_col_os
+def trap_eau(N):
+    a = 0
+    b = T_i
+    h = (b-a)/N
+    s = 0.5*f_eau(a) + 0.5*f_eau(b)
+    for k in range(1,N):
+        s += f_eau(a+k*h)
+    return (h*s)
+def trap_os(N):
+    a = 0
+    b = T_i
+    h = (b-a)/N
+    s = 0.5*f_os(a) + 0.5*f_os(b)
+    for k in range(1,N):
+        s += f_os(a+k*h)
+    return (h*s)
+print('''La portée des protons dans l'os est de '''+str(trap_os(1000))+''' [Kg/J*m^2]''')
+print('''La portée des protons dans l'os est de '''+str(trap_os(2000))+''' [Kg/J*m^2]''')
+print('''La portée des protons dans l'eau est de '''+str(trap_eau(1000))+''' [Kg/J*m^2]''')
+print('''La portée des protons dans l'eau est de '''+str(trap_eau(2000))+''' [Kg/J*m^2]''')
+tranches_eau = 1
+tranches_os = 1
+while True is True:
+    I_ii = trap_os(2*tranches_os)
+    I_i = trap_os(tranches_os)
+    eps = (1/3)*(I_ii-I_i)
+    tranches_eau += 1
+    if eps < 2.2e-16:
+        print('''Le nombre de tranches os est de '''+ str(tranches_os-1))
+        break
+while True is True:
+    I_ii = trap_eau(2*tranches_eau)
+    I_i = trap_eau(tranches_eau)
+    eps = (1/3)*(I_ii-I_i)
+    tranches_eau += 1
+    if eps < 2.2e-16:
+        print('''Le nombre de tranches eau est de '''+ str(tranches_eau-1))
+        break
 
-s = 0.5*f_eau(a) + 0.5*f_eau(b)
-for k in range(1,N):
-    s += f_eau(a+k*h)
-print(h*s)
+
+    
+
+#N=1000
+#a = 0
+#b = T_i
+#h = (b - a)/N
+
+#s = 0.5*f_eau(a) + 0.5*f_eau(b)
+#for k in range(1,N):
+#    s += f_eau(a+k*h)
+#print(h*s)
+
+#n=1000
+#c = 0
+#d = T_i
+#H = (d - c)/n
+
+#S = 0.5*f_os(c) + 0.5*f_os(d)
+#for i in range(1,n):
+#    S += f_os(c+i*H)
+
+#print(H*S)
---
+++ test2.py
@@ -0,0 +1,9 @@
+import numpy as numpy
+x=1
+b = 4
+while True is True:
+   x +=1
+   print(x)
+   if x >10:
+        print('bdhajsfa')
+        break
