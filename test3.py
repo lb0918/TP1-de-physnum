4cc18ee024a1f13ce73121e88c50edfe01fc0994
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-02-09 18:02:37 -0500

    few

--- test3.py
+++ test3.py
@@ -1,7 +1,18 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def test(t):
-    if t == 'yes':
-        print('réussi')
-test('yes')
+# importing the required module
+import timeit
+ 
+# code snippet to be executed only once
+mysetup = "from math import sqrt"
+ 
+# code snippet whose execution time is to be measured
+mycode = '''
+def example():
+    mylist = []
+    for x in range(100):
+        mylist.append(sqrt(x))
+'''
+ 
+# timeit statement
+print (timeit.timeit(setup = mysetup,
+                     stmt = mycode,
+                     number = 10000))
