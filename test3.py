0a73d6178e1eb59e9ea5aa5fe27b729059a1662c
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-02-10 18:37:08 -0500

    Plus propre jusqu'au 11

--- test3.py
+++ test3.py
@@ -1,18 +1,13 @@
-# importing the required module
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import moyal
+from scipy.stats import skew
+from scipy import integrate
 import timeit
- 
-# code snippet to be executed only once
-mysetup = "from math import sqrt"
- 
-# code snippet whose execution time is to be measured
-mycode = '''
-def example():
-    mylist = []
-    for x in range(100):
-        mylist.append(sqrt(x))
-'''
- 
-# timeit statement
-print (timeit.timeit(setup = mysetup,
-                     stmt = mycode,
-                     number = 10000))
+
+moy = moyal(loc = 150, scale = 4).rvs(size = 10000)
+plt.figure(figsize = (15, 15))
+plt.hist(moy, bins = 100)
+plt.show()
+a = skew(moy)
+print('''L'asymétrie de la distribution d'énergies de moyal est de '''+str(a)+'.')
