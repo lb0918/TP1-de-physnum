87fef2a0fcf32a4b7cd592871fc1a2265da939ab
lb0918 <lbstc@ulaval.ca>
2022-02-11 19:13:29 -0500

    efew

--- test3.py
+++ test3.py
@@ -5,9 +5,8 @@ from scipy.stats import skew
 from scipy import integrate
 import timeit
 
-moy = moyal(loc = 150, scale = 4).rvs(size = 10000)
-plt.figure(figsize = (15, 15))
-plt.hist(moy, bins = 100)
-plt.show()
-a = skew(moy)
-print('''L'asymétrie de la distribution d'énergies de moyal est de '''+str(a)+'.')
+for x in range(150,-1,-1):
+    print(x)
+l=[4,2,2,345,23]
+l.sort()
+print(l)
